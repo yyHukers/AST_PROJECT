de3bf4ef96f28a68de45be599c4a816b2e575ca8
hujiayuan <hjy9063@gmaile.com>
2022-10-09 23:29:51 +0800

    完善目录结构

---
+++ src/JavaScriptConfusion.ts
@@ -0,0 +1,17 @@
+import { IJavaScriptConfusion } from "./interfaces/IJavaScriptConfusion";
+
+/**
+ * 混淆
+ * @version 1.0.0
+ */
+export class JavaScriptConfusion implements IJavaScriptConfusion {
+	public static obfuscate(sourceCode: string) {}
+	private static instance = null;
+}
+
+
+
+
+
+
+
